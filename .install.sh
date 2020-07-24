9313a34e68ba26058ed66359585fb4c8d08842f8
Sidharth-Prabhakaran <64224697+Sidharth-Prabhakaran@users.noreply.github.com>
2020-07-24 23:21:26 +0530

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -9,7 +9,7 @@ fi
 echo "\n" | ssh-keygen -q -t rsa -P "" -f /root/.ssh/id_rsa
 cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
-echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
+echo " StrictHostKeyChecking no" >> /etc/ssh/ssh_config
 echo 'Defaults        secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/opt/mdp/hbase-2.0.0/bin:/opt/mdp/hadoop-2.7.6/sbin:/opt/mdp/hadoop-2.7.6/bin:/opt/mdp/pig-0.16.0/bin:/opt/mdp/apache-hive-2.1.0-bin/bin"' > /etc/sudoers.d/exconf
 echo 'Defaults env_keep += "JAVA_HOME"' >> /etc/sudoers.d/exconf
 service ssh restart
